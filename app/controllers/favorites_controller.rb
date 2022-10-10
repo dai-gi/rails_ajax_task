ce289d2957cbfa27f1b789714ae7b97f79bfebc7
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-10-10 23:11:09 +0900

    Modify: お気に入り機能の非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,27 @@
 class FavoritesController < ApplicationController
+  before_action :set_favorite, only: %i[ destroy ]
+  before_action :set_blog
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    @favorite = Favorite.create(user_id: current_user.id, blog_id: @blog.id)
+    respond_to do |format|
+      format.js { render :create }
+    end
   end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    @favorite.destroy
+    respond_to do |format|
+      format.js { render :destroy }
+    end
   end
 
+  private
+  def set_favorite
+    @favorite = Favorite.find(params[:id])
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
 end
